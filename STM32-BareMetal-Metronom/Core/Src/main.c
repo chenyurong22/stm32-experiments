e638cd09065e9c04d359065baa8dc84aa9cee29d
Jonas Thaun <thaunjonas@gmail.com>
2026-06-19 23:00:15 +0200

    Metronom Project Setup

---
+++ STM32-BareMetal-Metronom/Core/Src/main.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+void SystemClock_Config(void);
+
+int main(void)
+{
+  LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_PWR);
+  NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+  NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 15, 0));
+  SystemClock_Config();
+
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+
+  GPIOA->MODER &= ~GPIO_MODER_MODER1;
+  GPIOA->MODER |= GPIO_MODER_MODER1_0;
+
+  while (1)
+  {
+    GPIOA->BSRR = GPIO_BSRR_BS_1;
+
+    for (volatile uint32_t i = 0; i < 2000000; i++)
+    {
+    }
+    GPIOA->BSRR = GPIO_BSRR_BR_1;
+
+    for (volatile uint32_t i = 0; i < 2000000; i++)
+    {
+    }
+  }
+}
+
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_5);
+  while (LL_FLASH_GetLatency() != LL_FLASH_LATENCY_5)
+  {
+  }
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+  LL_RCC_HSE_Enable();
+
+  /* Wait till HSE is ready */
+  while (LL_RCC_HSE_IsReady() != 1)
+  {
+  }
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSE, LL_RCC_PLLM_DIV_4, 168, LL_RCC_PLLP_DIV_2);
+  LL_RCC_PLL_Enable();
+
+  /* Wait till PLL is ready */
+  while (LL_RCC_PLL_IsReady() != 1)
+  {
+  }
+  while (LL_PWR_IsActiveFlag_VOS() == 0)
+  {
+  }
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_4);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_2);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+  /* Wait till System clock is ready */
+  while (LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+  }
+  LL_Init1msTick(168000000);
+  LL_SetSystemCoreClock(168000000);
+}
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
