59d02bcb076f1e5bd83d50b6670a9248ea5df07c
Jonas Thaun <thaunjonas@gmail.com>
2026-06-20 21:36:23 +0200

    timer, button, adc poti working

--- STM32-BareMetal-Metronom/Core/Src/main.c
+++ STM32-BareMetal-Metronom/Core/Src/main.c
@@ -2,6 +2,19 @@
 
 void SystemClock_Config(void);
 
+volatile uint8_t metronom_tick_requestet = 0;
+
+uint16_t ADC_Read_Channel2(void)
+{
+  ADC1->CR2 |= ADC_CR2_SWSTART;
+
+  while (!(ADC1->SR & ADC_SR_EOC))
+  {
+  }
+
+  return (uint16_t)(ADC1->DR);
+}
+
 int main(void)
 {
   LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_SYSCFG);
@@ -10,23 +23,84 @@ int main(void)
   NVIC_SetPriority(SysTick_IRQn, NVIC_EncodePriority(NVIC_GetPriorityGrouping(), 15, 0));
   SystemClock_Config();
 
-  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
+  RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // GPIO Port A
+  RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;  // Timer 2
+  RCC->APB2ENR |= RCC_APB2ENR_ADC1EN; //adc1
 
+  // PA1 alternate function 
   GPIOA->MODER &= ~GPIO_MODER_MODER1;
-  GPIOA->MODER |= GPIO_MODER_MODER1_0;
+  GPIOA->MODER |= GPIO_MODER_MODER1_1;
+
+  GPIOA->AFR[0] &= ~(0xF << 4);
+  GPIOA->AFR[0] |= (1 << 4);
+
+  // PA2 analog input
+  GPIOA->MODER |= GPIO_MODER_MODER2;
+
+  // PA0 input
+  GPIOA->MODER &= ~GPIO_MODER_MODER0;
+
+  //exti0 port a config
+  SYSCFG->EXTICR[0] &= ~SYSCFG_EXTICR1_EXTI0_PA;
+  EXTI->RTSR |= EXTI_RTSR_TR0; //rising trigger
+  EXTI->IMR |= EXTI_IMR_MR0; //interrupt mask
+  NVIC_EnableIRQ(EXTI0_IRQn);
+
+  // channel 2 start
+  ADC1->SQR3 = 2;
+  ADC1->CR2 |= ADC_CR2_ADON;
+
+  TIM2->PSC = 84000 - 1;
+  TIM2->ARR = 1000 - 1;
+
+  TIM2->CCR2 = 50;
+  TIM2->CCMR1 &= ~TIM_CCMR1_OC2M;
+  TIM2->CCMR1 |= (TIM_CCMR1_OC2M_1 | TIM_CCMR1_OC2M_2 | TIM_CCMR1_OC2PE);
+  TIM2->CCER |= TIM_CCER_CC2E;
+
+  // enbale interrupt
+  // TIM2->DIER |= TIM_DIER_UIE;
+  // NVIC_EnableIRQ(TIM2_IRQn);
+
+  // update & start
+  TIM2->EGR |= TIM_EGR_UG;
+  TIM2->CR1 |= TIM_CR1_CEN;
+
+  uint16_t poti_wert = 0;
 
   while (1)
   {
-    GPIOA->BSRR = GPIO_BSRR_BS_1;
+    poti_wert = ADC_Read_Channel2();
 
-    for (volatile uint32_t i = 0; i < 2000000; i++)
-    {
-    }
-    GPIOA->BSRR = GPIO_BSRR_BR_1;
+    uint32_t bpm = 40 + ((poti_wert * 200) / 4095);
+    uint32_t arr_wert = (60000 / bpm) - 1;
+    TIM2->ARR = arr_wert;
 
-    for (volatile uint32_t i = 0; i < 2000000; i++)
+    if (TIM2->CNT > arr_wert)
     {
+      TIM2->EGR |= TIM_EGR_UG;
     }
+  }
+}
+
+// void TIM2_IRQHandler(void)
+// {
+//   // test update interrupt flat und status register
+//   if (TIM2->SR & TIM_SR_UIF)
+//   {
+//     // deactivate interrupt
+//     TIM2->SR &= ~TIM_SR_UIF;
+
+//     metronom_tick_requestet = 1;
+//   }
+// }
+
+void EXTI0_IRQHandler(void) {
+  //pending
+  if(EXTI->PR & EXTI_PR_PR0) {
+    EXTI->PR = EXTI_PR_PR0; //clear by writing 1
+
+    TIM2->CR1 ^= TIM_CR1_CEN; //counter enable
   }
 }
 
